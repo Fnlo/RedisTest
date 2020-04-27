487ba9addac9da46ef0e2b52ffb6d6a6ded669bc
Fnlo <fenglu@highgo.com>
2020-04-27 14:56:17 +0800

    comments. process introduction for SolveCacheAvalanche

--- src/main/java/com/Fnlo/Redis/CacheProblem/CacheAvalanche/SolveCacheAvalanche.java
+++ src/main/java/com/Fnlo/Redis/CacheProblem/CacheAvalanche/SolveCacheAvalanche.java
@@ -6,6 +6,12 @@ import redis.clients.jedis.Jedis;
  * @author ：fenglu<fnlo1995@gmail.com>
  * @date ：Created in 2020/4/27 14:12
  * @description：本类介绍了对Redis雪崩时的一种处理方式，通过使用SETNX加锁避免雪崩
+ *      对于每个线程处理业务的流程如下:
+ *      1. 判断缓存数据是否过期
+ *      2. 数据过期时，使用SETNX(锁，超时时间戳)的方式不停尝试获取该锁。(若存在持有锁的线程异常导致锁未释放，
+ *      为避免导致死锁，其他线程会根据Lock的超时时间 getSet新的值，从而获得该锁的拥有权)
+ *      3. 获取锁之后，首先尝试从缓存中获取一次数据，避免获取锁的过程中有其他的线程已经更新缓存。若无数据，load db，
+ *      然后更新缓存，释放锁。
  * @modified By：
  * @version: $version$
  */
@@ -52,9 +58,9 @@ public class SolveCacheAvalanche {
                         String temp;
                         //如果某个线程异常退出，但是未能del(Lock)，导致死锁，所以为Lock对象添加超时的时间戳
                         if ((now >  Long.parseLong((lockTimeStamp == null || lockTimeStamp.length() <= 0 ) ? "0" : lockTimeStamp)
-                                && now > Long.parseLong(((temp = jedis.getSet(LOCK, (now + TIMEOUT) + "")) == null || temp.length() <= 0) ? "0" : temp))){
-                            System.out.println(Thread.currentThread().getName() + "超时，getset锁，并添加新的时间戳");
-                            break;
+                                    && now > Long.parseLong(((temp = jedis.getSet(LOCK, (now + TIMEOUT) + "")) == null || temp.length() <= 0) ? "0" : temp))){
+                                System.out.println(Thread.currentThread().getName() + "超时，getset锁，并添加新的时间戳");
+                                break;
                         } else {//一二三四 再来一次
                             Thread.sleep(100);
                         }
